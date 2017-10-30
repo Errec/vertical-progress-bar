44532ab4f008d8a67a3071664e2f6906ca7a6486
Raniro Coelho <errecme@gmail.com>
2017-10-30 20:28:49 -0200

    Add ease animation to bullet transform

--- js/main.js
+++ js/main.js
@@ -5,7 +5,6 @@ var placeHolders = document.querySelectorAll('.progress__bullet-placeholder-item
 
 var MAX_ITEMS = placeHolders.length;
 var START_H   = 1;
-
 var step = 0;
 
 next.addEventListener('click', stepForward);
@@ -13,12 +12,12 @@ back.addEventListener('click', stepBack);
 
 function stepForward() {
   if (step < MAX_ITEMS - 1) {
-    bullet.style.top = (++step * 10 + START_H) + '%';
+    bullet.style.transform = 'translateY(' + (++step * 125 + START_H) + '%)';
   }
 }
 
 function stepBack() {
   if (step > 0) {
-    bullet.style.top = (--step * 10 + START_H) + '%';
+    bullet.style.transform = 'translateY(' + (--step * 125 + START_H) + '%)';
   }
 }
