7e875262827245a56c1339c4b85e4a29c13fa4ee
Raniro Coelho <errecme@gmail.com>
2017-10-30 23:06:05 -0200

    Add hide animation for placeholders

--- js/main.js
+++ js/main.js
@@ -1,23 +1,36 @@
-var next         = document.querySelector('.btns__next');
-var back         = document.querySelector('.btns__back');
-var bullet       = document.querySelector('.progress__bullet');
-var placeHolders = document.querySelectorAll('.progress__bullet-placeholder-item');
+var next              = document.querySelector('.btns__next');
+var back              = document.querySelector('.btns__back');
+var bullet            = document.querySelector('.progress__bullet');
+var placeholders      = document.querySelectorAll('.progress__bullet-placeholder-item');
+var placeholderBullet = document.querySelectorAll('.progress__bullet-placehoder');
 
-var MAX_ITEMS = placeHolders.length;
+var MAX_ITEMS = placeholders.length;
 var START_H   = 1;
-var step = 0;
+var step      = 0;
 
 next.addEventListener('click', stepForward);
 back.addEventListener('click', stepBack);
 
 function stepForward() {
   if (step < MAX_ITEMS - 1) {
-    bullet.style.transform = 'translateY(' + (++step * 125 + START_H) + '%)';
+    bullet.style.top = (++step * 10 + START_H) + '%';
   }
+  renderPlaceholder();
 }
 
 function stepBack() {
   if (step > 0) {
-    bullet.style.transform = 'translateY(' + (--step * 125 + START_H) + '%)';
+    bullet.style.top = (--step * 10 + START_H) + '%';
   }
+  renderPlaceholder();
+}
+
+function renderPlaceholder() {
+  Array.prototype.forEach.call(placeholderBullet, function(element, index) {
+    if (index < step) {
+      element.classList.add('progress__bullet-placehoder--hide');
+    } else {
+      element.classList.remove('progress__bullet-placehoder--hide');
+    }
+  });
 }
