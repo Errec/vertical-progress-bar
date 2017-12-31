90cb6891e92324a56b78b74f7046814c58c0db16
Raniro Coelho <errecme@gmail.com>
2017-12-30 23:47:17 -0200

    Add lodash throttle to btn events

--- js/main.js
+++ js/main.js
@@ -6,28 +6,31 @@ var placeholderBullet = document.querySelectorAll('.progress__bullet-placehoder'
 var barBullet         = document.querySelector('.progress__bar-bullet');
 
 var MAX_ITEMS = placeholders.length;
+var THROTTLE_TIME = 400;
 var step      = 0;
 
-next.addEventListener('click', stepForward);
-back.addEventListener('click', stepBack);
+next.addEventListener('click', _.throttle(stepForward, THROTTLE_TIME));
+back.addEventListener('click', _.throttle(stepBack, THROTTLE_TIME));
 
 function stepForward() {
   if (step < MAX_ITEMS - 1) {
+    toggleDisableBtn(next);
     bar.style.height = (++step * 10) + '%';
     barBullet.style.transform = 'rotate(' + 120 * step + 'deg)';
+    renderPlaceholder();
   }
-  renderPlaceholder();
 }
 
-function stepBack() {
+function stepBack () {
   if (step > 0) {
+    toggleDisableBtn(back);
     bar.style.height = (--step * 10) + '%';
     barBullet.style.transform = 'rotate(' + 120 * step + 'deg)';
+    renderPlaceholder();
   }
-  renderPlaceholder();
 }
 
-function renderPlaceholder() {
+function renderPlaceholder () {
   Array.prototype.forEach.call(placeholderBullet, function(element, index) {
     if (index < step) {
       element.classList.add('progress__bullet-placehoder--hide');
@@ -35,4 +38,11 @@ function renderPlaceholder() {
       element.classList.remove('progress__bullet-placehoder--hide');
     }
   });
+}
+
+function toggleDisableBtn (btn) {
+  btn.disabled = true;
+  setTimeout(function () {
+    btn.disabled = false;
+  }, THROTTLE_TIME)
 }
